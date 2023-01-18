b2d1e2ca703466706fb8ac7756ede55f35a59780
hsuanhl <shuanhui.l@gmail.com>
2023-01-18 12:04:21 +0800

    第一版

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+
+$(document).ready(function(){
+    $('.slider').slick({
+        centerMode: true,
+        centerPadding: '100px',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: true
+    });
+  });  
+  
