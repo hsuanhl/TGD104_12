d745cf1ff655f5a403d89f0c1d624ec9f06706be
hsuanhl <shuanhui.l@gmail.com>
2023-01-25 19:16:17 +0800

    50percent

---
+++ test.js
@@ -0,0 +1,6 @@
+const month = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August','September', 'October', 'November', 'December']
+
+
+isLeapYear = (year) => {
+    return (year % 4 === 0 && year % =)
+}
