eaf9bd6ae6615cb0f3821070394effc54785c095
hsuanhl <shuanhui.l@gmail.com>
2023-01-29 00:56:22 +0800

    d2

---
+++ js/main.js
@@ -0,0 +1,49 @@
+
+
+// $(document).ready(function(){  }); 
+
+    // $('.slider').slick({
+    //     centerMode: true,
+    //     centerPadding: '100px',
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //     dots: true,
+    //     arrows: true
+    // });
+
+
+$('.slider').slick({
+            centerMode: true,
+            centerPadding: '280px',
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            arrows: true
+        });
+
+$('.flooronelist').slick({
+            centerMode: true,
+            centerPadding: '10px',
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            arrows: true,
+        });
+
+$('.exhpiclist').slick({
+            // centerMode: true,
+            // centerPadding: '10px',
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            arrows: true,
+        });
+
+$('.evpiclist').slick({
+            // centerMode: true,
+            // centerPadding: '10px',
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            arrows: true,
+        });
+
+
+
+  
--- test.js
+++
@@ -1,6 +0,0 @@
-const month = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August','September', 'October', 'November', 'December']
-
-
-isLeapYear = (year) => {
-    return (year % 4 === 0 && year % =)
-}
