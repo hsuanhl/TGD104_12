45760b20d272c8693725c4ed6edca81852461a33
hsuanhl <shuanhui.l@gmail.com>
2023-02-20 21:31:45 +0800

    230220

--- js/main.js
+++ js/main.js
@@ -18,15 +18,14 @@ $('.banner').each(function(){
     }
 })
 
-        let myVar;
-
-        function myFunction() {
-        myVar = setTimeout(showPage, 4000);
+    let myVar;
+    function myFunction() {
+    myVar = setTimeout(showPage, 4000);
         }
         
-        function showPage() {
-        document.getElementById("loader").style.display = "none";
-        document.getElementById("myDiv").style.display = "block";
+    function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("myDiv").style.display = "block";
         } 
 
 $('.burger').click(function(e) {
